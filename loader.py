763c291128cc0e9e05ce2d6a38ea0825b633720d
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-03 20:04:16 +0300

    created Dispatcher object

--- loader.py
+++ loader.py
@@ -1,6 +1,7 @@
-from aiogram import Bot, types
+from aiogram import Bot, types, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from envfileparser import get_env
 
 bot = Bot(token=get_env('API_TOKEN'), parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
