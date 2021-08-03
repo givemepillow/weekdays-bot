70c895f6edb63ed8f618a66be561042033110853
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-03 20:10:20 +0300

    created select_month message handler - state Year

--- handlers/ymw_handler.py
+++ handlers/ymw_handler.py
@@ -1,4 +1,5 @@
 from aiogram import types
+from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Command
 
 from loader import dp
@@ -9,3 +10,11 @@ from states.ymw import YearMonthWeekday
 async def select_year(message: types.Message):
     await message.answer("Выбирете год.")
     await YearMonthWeekday.first()
+
+
+@dp.message_handler(state=YearMonthWeekday.Year)
+async def select_month(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['year'] = message.text
+    await message.answer("Выберите месяц.")
+    await YearMonthWeekday.next()
