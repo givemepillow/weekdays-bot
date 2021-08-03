f88eb99f82b182d519204e32c4b9fac469c5d221
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-03 20:03:32 +0300

    created Bot object and MemoryStorage

---
+++ loader.py
@@ -0,0 +1,6 @@
+from aiogram import Bot, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from envfileparser import get_env
+
+bot = Bot(token=get_env('API_TOKEN'), parse_mode=types.ParseMode.HTML)
+storage = MemoryStorage()
