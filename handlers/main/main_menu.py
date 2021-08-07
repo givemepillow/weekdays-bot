dca9ff5fbe9c5e3ccf962440b5e8a6a0ab2b83d4
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-07 12:15:52 +0300

    Created main menu handler.

---
+++ handlers/main/main_menu.py
@@ -0,0 +1,11 @@
+from aiogram.dispatcher.filters import Command, Text
+from aiogram.types import Message
+
+from markup.text import menu, buttons
+from loader import dp
+
+
+@dp.message_handler(Text(equals=buttons.main_menu_btn.text), state='*')
+@dp.message_handler(Command("start"), state='*')
+async def main_menu(message: Message):
+    await message.answer("Главное меню. Выбирете что хотите сделать:", reply_markup=menu.main_menu)
