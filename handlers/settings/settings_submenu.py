298726dee49d589356be76d1c0bb0a9d7dad6bde
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-07 19:53:38 +0300

    Handels main_menu and sets settings_submenu state.

--- handlers/settings/settings_submenu.py
+++ handlers/settings/settings_submenu.py
@@ -3,8 +3,10 @@ from aiogram.types import Message
 
 from markups.text import menu, buttons
 from loader import dp
+from states import Menus
 
 
-@dp.message_handler(Text(equals=buttons.settings_btn.text), state=None)
+@dp.message_handler(Text(equals=buttons.settings_btn.text), state=Menus.main_menu)
 async def settings_submenu(message: Message):
+    await Menus.settings_submenu.set()
     await message.answer(f"Что имеено вы хотите сделать?", reply_markup=menu.settings_submenu)
