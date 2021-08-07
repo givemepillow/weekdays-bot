34ae21f7b5517f445eb3e2039fc2a020a928a111
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-07 20:37:20 +0300

    Handler for go_back_btn. Works only with add_holidays_submenu state.

--- handlers/settings/settings_submenu.py
+++ handlers/settings/settings_submenu.py
@@ -1,12 +1,13 @@
 from aiogram.dispatcher.filters import Text
 from aiogram.types import Message
 
-from markups.text import menu, buttons
 from loader import dp
+from markups.text import menu, buttons
 from states import Menus
 
 
-@dp.message_handler(Text(equals=buttons.settings_btn.text), state=Menus.main_menu)
+@dp.message_handler(Text(equals=buttons.go_back_btn.text), state=[Menus.add_holidays_submenu])
+@dp.message_handler(Text(equals=buttons.settings_btn.text), state=[Menus.main_menu])
 async def settings_submenu(message: Message):
     await Menus.settings_submenu.set()
     await message.answer(f"Что имеено вы хотите сделать?", reply_markup=menu.settings_submenu)
