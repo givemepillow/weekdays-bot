56a90d2f41cc0ddeb150cfc99c6f707cdb774f89
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-07 12:17:16 +0300

    Created represent submenu handler.

---
+++ handlers/represent/represent_submenu.py
@@ -0,0 +1,10 @@
+from aiogram.dispatcher.filters import Text
+from aiogram.types import Message
+
+from markup.text import menu, buttons
+from loader import dp
+
+
+@dp.message_handler(Text(equals=buttons.represent_btn.text), state=None)
+async def represent_submenu(message: Message):
+    await message.answer(f"В каком виде представить данные?", reply_markup=menu.represent_submenu)
