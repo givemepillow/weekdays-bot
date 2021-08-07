0a828aa50098a10473908a4797dd38aec8839bbb
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-07 12:17:24 +0300

    Created settings submenu handler.

---
+++ handlers/settings/settings_submenu.py
@@ -0,0 +1,10 @@
+from aiogram.dispatcher.filters import Text
+from aiogram.types import Message
+
+from markup.text import menu, buttons
+from loader import dp
+
+
+@dp.message_handler(Text(equals=buttons.settings_btn.text), state=None)
+async def settings_submenu(message: Message):
+    await message.answer(f"Что имеено вы хотите сделать?", reply_markup=menu.settings_submenu)
