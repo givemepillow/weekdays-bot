d197181836ad78895eb972ac042877a45e0d4ada
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-07 19:51:25 +0300

    Handle all Menus states and set main_menu state.

--- handlers/main/main_menu.py
+++ handlers/main/main_menu.py
@@ -1,11 +1,15 @@
+from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Command, Text
 from aiogram.types import Message
 
-from markups.text import menu, buttons
 from loader import dp
+from markups.text import menu, buttons
+from states import Menus
 
 
+@dp.message_handler(Text(equals=buttons.go_back_btn.text), state=Menus.all_states)
 @dp.message_handler(Text(equals=buttons.main_menu_btn.text), state='*')
 @dp.message_handler(Command("start"), state='*')
 async def main_menu(message: Message):
     await message.answer("Главное меню. Выбирете что хотите сделать:", reply_markup=menu.main_menu)
+    await Menus.main_menu.set()
