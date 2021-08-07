211be7cfa45e84f0fa8b9cd3b8e2d662b63c0a3a
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-07 12:17:40 +0300

    Created representations handlers.

---
+++ handlers/represent/representation_one.py
@@ -0,0 +1,10 @@
+from aiogram.dispatcher.filters import Text
+from aiogram.types import Message
+
+from markup.text import menu, buttons
+from loader import dp
+
+
+@dp.message_handler(Text(equals=buttons.first_view_btn.text), state=None)
+async def representation_one(message: Message):
+    await message.answer(f"ВИД1", reply_markup=menu.default_submenu)
