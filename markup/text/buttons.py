771c0a7d95fc02605150857c34199c2030b2b297
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-07 12:14:07 +0300

    Created text buttons.

---
+++ markup/text/buttons.py
@@ -0,0 +1,10 @@
+from aiogram.types import KeyboardButton
+
+represent_btn = KeyboardButton('Отобразить данные')
+settings_btn = KeyboardButton('Настройки')
+main_menu_btn = KeyboardButton('Главное меню')
+first_view_btn = KeyboardButton('Вид 1')
+second_view_btn = KeyboardButton('Вид 2')
+add_holidays_btn = KeyboardButton('Добавить выходные')
+reset_holidays_btn = KeyboardButton('Удалить выходные')
+go_back_btn = KeyboardButton('Назад')
