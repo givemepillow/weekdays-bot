4aeabcecee3bed4644d29dc99e93fde3ed31c3c8
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-09 19:51:20 +0300

    Added text reply_markup.

--- handlers/settings/calendar.py
+++ handlers/settings/calendar.py
@@ -1,8 +1,8 @@
-from markups.inline.calendar import InlineCalendar, calendar_cb, storage
 from aiogram.dispatcher.filters import Text
 from aiogram.types import Message
 
 from loader import dp
+from markups.inline.calendar import InlineCalendar
 from markups.text import menu, buttons
 from states import Menus
 
@@ -12,4 +12,5 @@ calendar = InlineCalendar()
 @dp.message_handler(Text(equals=buttons.edit_holidays_btn.text), state=[Menus.settings_submenu, None])
 async def open_calendar_handler(message: Message):
     await Menus.calendar.set()
-    await message.answer("Отметье выходные: ", reply_markup=await calendar())
+    await message.answer(text="Редактирование выходных.", reply_markup=menu.edit_calendar_submenu)
+    await message.answer(text='Отметьте выходыне:', reply_markup=await calendar())
