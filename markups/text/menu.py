ca9ba269b6017cfecbc442c86470ffa0ccc72366
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-09 19:50:02 +0300

    Added calendar_submenu.

--- markups/text/menu.py
+++ markups/text/menu.py
@@ -4,7 +4,8 @@ from .buttons import (
     represent_btn, settings_btn, main_menu_btn,
     first_view_btn, second_view_btn,
     edit_holidays_btn, reset_holidays_btn,
-    go_back_btn, info_btn
+    go_back_btn, info_btn, edit_holidays_cancel_btn,
+    edit_holidays_save_btn
 )
 
 # Main menu.
@@ -30,3 +31,9 @@ default_submenu = ReplyKeyboardMarkup(
     resize_keyboard=True,
     row_width=2
 ).add(main_menu_btn, go_back_btn)
+
+# Edit calendar menu.
+edit_calendar_submenu = ReplyKeyboardMarkup(
+    resize_keyboard=True,
+    row_width=2
+).add(edit_holidays_save_btn, edit_holidays_cancel_btn, go_back_btn)
