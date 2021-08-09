6582a4865afc1dc8e9c76e7dfa71fe8134894361
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-09 19:00:51 +0300

    Updated main_menu handler.

--- handlers/main/main_menu.py
+++ handlers/main/main_menu.py
@@ -6,8 +6,8 @@ from markups.text import menu, buttons
 from states import Menus
 
 
-@dp.message_handler(state=None)  # Show the main menu if something went wrong.
-@dp.message_handler(Text(equals=buttons.go_back_btn.text), state=[Menus.settings_submenu, Menus.represent_submenu])
+@dp.message_handler(Text(equals=buttons.go_back_btn.text),
+                    state=[Menus.settings_submenu, Menus.represent_submenu, None])
 @dp.message_handler(Command("start"), state='*')
 async def main_menu(message: Message):
     await message.answer("Главное меню. Выбирете что хотите сделать:", reply_markup=menu.main_menu)
