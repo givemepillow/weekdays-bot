5a6175427f2a9affa91b04c7cc14330c99dd1b29
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-09 18:52:22 +0300

    Created new file.

---
+++ handlers/settings/calendar.py
@@ -0,0 +1,9 @@
+from markups.inline.calendar import InlineCalendar, calendar_cb, storage
+from aiogram.dispatcher.filters import Text
+from aiogram.types import Message
+
+from loader import dp
+from markups.text import menu, buttons
+from states import Menus
+
+
