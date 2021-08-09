e4867230e44a6c618fab009823736a6b4450afc4
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-10 01:30:31 +0300

    Added are_u_sure and reset_complete handlers.

--- handlers/settings/settings_submenu.py
+++ handlers/settings/settings_submenu.py
@@ -1,13 +1,31 @@
+from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
 from aiogram.types import Message
 
-from loader import dp
+from loader import dp, bot
 from markups.text import menu, buttons
 from states import Menus
 
 
-@dp.message_handler(Text(equals=buttons.go_back_btn.text), state=Menus.calendar)
+@dp.message_handler(Text(equals=[buttons.go_back_btn.text, buttons.no_btn.text]),
+                    state=[Menus.calendar, Menus.reset_complete, Menus.are_u_sure])
 @dp.message_handler(Text(equals=buttons.settings_btn.text), state=[Menus.main_menu, None])
-async def settings_submenu(message: Message):
+async def settings_submenu(message: Message, state: FSMContext):
+    async with state.proxy() as data:
+        if 'calendar_id' in data and data['calendar_id'] is not None:
+            await bot.delete_message(chat_id=message.chat.id, message_id=data['calendar_id'])
+            data['calendar_id'] = None
     await Menus.settings_submenu.set()
     await message.answer(f"Вы перешли в настройки.", reply_markup=menu.settings_submenu)
+
+
+@dp.message_handler(Text(equals=buttons.reset_holidays_btn.text), state=Menus.settings_submenu)
+async def are_u_sure(message: Message):
+    await Menus.are_u_sure.set()
+    await message.answer(f"Вы вы точно хотите очистить календарь?.", reply_markup=menu.are_u_sure_submenu)
+
+
+@dp.message_handler(Text(equals=buttons.yes_btn.text), state=Menus.are_u_sure)
+async def reset_complete(message: Message):
+    await Menus.reset_complete.set()
+    await message.answer(f"Все выходные сброшены.", reply_markup=menu.go_back_submenu)
