58d390eb5c31a8c8bce3d37b6071abc12ede7dfb
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-09 19:16:19 +0300

    Fixed handlers texts and states.

--- handlers/settings/settings_submenu.py
+++ handlers/settings/settings_submenu.py
@@ -6,7 +6,8 @@ from markups.text import menu, buttons
 from states import Menus
 
 
+@dp.message_handler(Text(equals=buttons.go_back_btn.text), state=Menus.calendar)
 @dp.message_handler(Text(equals=buttons.settings_btn.text), state=[Menus.main_menu, None])
 async def settings_submenu(message: Message):
     await Menus.settings_submenu.set()
-    await message.answer(f"Что имеено вы хотите сделать?", reply_markup=menu.settings_submenu)
+    await message.answer(f"Вы пережли в настройки. Что хотите сделать?", reply_markup=menu.settings_submenu)
