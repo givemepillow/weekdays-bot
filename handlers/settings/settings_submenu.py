45b872fd84a1e3ec0b3b752ac16202f55cabce33
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-09 15:33:03 +0300

    Updated handlers for settings_submenu().

--- handlers/settings/settings_submenu.py
+++ handlers/settings/settings_submenu.py
@@ -6,7 +6,6 @@ from markups.text import menu, buttons
 from states import Menus
 
 
-@dp.message_handler(Text(equals=buttons.go_back_btn.text), state=[Menus.add_holidays_submenu])
 @dp.message_handler(Text(equals=buttons.settings_btn.text), state=[Menus.main_menu, None])
 async def settings_submenu(message: Message):
     await Menus.settings_submenu.set()
