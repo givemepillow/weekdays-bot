b4025b6a7cb0096d28d008bfcb7da2bbfbf738cc
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-09 18:54:33 +0300

    Created open_calendar_handler().

--- handlers/settings/calendar.py
+++ handlers/settings/calendar.py
@@ -6,4 +6,9 @@ from loader import dp
 from markups.text import menu, buttons
 from states import Menus
 
+calendar = InlineCalendar()
 
+
+@dp.message_handler(Text(equals=buttons.edit_holidays_btn.text), state=Menus.settings_submenu)
+async def open_calendar_handler(message: Message):
+    await message.answer("Отметье выходные: ", reply_markup=await calendar())
