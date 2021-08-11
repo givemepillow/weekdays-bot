ee54105636a5096e95351005064b5c8a2d28022c
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-11 21:59:27 +0300

    Updated handler.

--- handlers/represent/represent_submenu.py
+++ handlers/represent/represent_submenu.py
@@ -7,7 +7,11 @@ from states import Menus
 
 
 @dp.message_handler(Text(equals=[buttons.represent_btn.text, buttons.go_back_btn.text]),
-                    state=[Menus.main_menu, Menus.representation_months, Menus.representation_months, None])
+                    state=[Menus.main_menu, Menus.representation_months,
+                           Menus.representation_months,
+                           Menus.representation_holidays,
+                           None
+                           ])
 async def represent_submenu(message: Message):
     await Menus.represent_submenu.set()
     await message.answer(f"В каком виде представить данные?", reply_markup=menu.represent_submenu)
