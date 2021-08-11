0ac6ef79dcd3242b8556bf67afd377ff457e096c
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-11 23:59:54 +0300

    New is_user_exists() method.

--- database/database.py
+++ database/database.py
@@ -8,6 +8,11 @@ class Database:
         self.db_name = db_name
         self.table_name = table_name
 
+    async def is_user_exists(self, user_id):
+        async with aiosqlite.connect(self.db_name) as db:
+            user = await db.execute_fetchall(f"SELECT COUNT(*) FROM {self.table_name} WHERE user_id = {user_id};")
+            return False if user[0][0] == 0 else True
+
     async def create(self):
         async with aiosqlite.connect(self.db_name) as db:
             await db.execute(f"CREATE TABLE IF NOT EXISTS {self.table_name} ("
@@ -18,8 +23,7 @@ class Database:
 
     async def save_user_holidays(self, user_id: int, user_holidays: dict):
         async with aiosqlite.connect(self.db_name) as db:
-            user = await db.execute_fetchall(f"SELECT COUNT(*) FROM {self.table_name} WHERE user_id = {user_id};")
-            if user[0][0] == 0:
+            if not await self.is_user_exists(user_id):
                 await db.execute(
                     f"INSERT INTO {self.table_name} (user_id, holidays) VALUES (?, ?);",
                     (user_id, str(json.dumps(user_holidays))))
@@ -32,8 +36,9 @@ class Database:
 
     async def get_user_holidays(self, user_id: int):
         async with aiosqlite.connect(self.db_name) as db:
-            holidays = await db.execute_fetchall(f"SELECT holidays FROM {self.table_name} WHERE user_id = {user_id};")
-            if len(holidays) == 0:
-                return []
-            else:
+            if await self.is_user_exists(user_id):
+                holidays = await db.execute_fetchall(
+                    f"SELECT holidays FROM {self.table_name} WHERE user_id = {user_id};")
                 return json.loads(holidays[0][0])
+            else:
+                return []
