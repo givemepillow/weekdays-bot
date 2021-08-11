63789b6720c64b8f485a6913757eeb0213e81d27
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-11 15:33:32 +0300

    go_back_submenu -> edit_calendar_submenu. Also updated logic of state.proxy() user holidays storing.

--- handlers/settings/open_calendar.py
+++ handlers/settings/open_calendar.py
@@ -2,7 +2,7 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
 from aiogram.types import Message
 
-from loader import dp
+from loader import dp, db
 from markups.inline.calendar import InlineCalendar
 from markups.text import buttons, menu
 from states import Menus
@@ -11,11 +11,10 @@ from states import Menus
 @dp.message_handler(Text(equals=buttons.edit_holidays_btn.text), state=[Menus.settings_submenu, None])
 async def open_calendar(message: Message, state: FSMContext):
     await Menus.calendar.set()
-    await message.answer(text="Редактирование выходных.", reply_markup=menu.go_back_submenu)
+    await message.answer(text="Редактирование выходных.", reply_markup=menu.edit_calendar_submenu)
     user_id = message.from_user.id
     async with state.proxy() as data:
-        if user_id not in data.keys():
-            data[user_id] = []
+        data[user_id] = await db.get_user_holidays(user_id)
         cal_msg = await message.answer(text='Отметьте выходыне:',
                                        reply_markup=await InlineCalendar()(storage=data[user_id]))
         data['calendar_id'] = cal_msg.message_id
