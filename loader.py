e989e6cb8cbfe11b4cb45480f37ae29c6e616250
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-11 15:37:55 +0300

    Instance of Database.

--- loader.py
+++ loader.py
@@ -1,7 +1,9 @@
 from aiogram import Bot, types, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from envfileparser import get_env
+from database import Database
 
 bot = Bot(token=get_env('API_TOKEN'), parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
+db = Database(f"database/{get_env('DB_NAME')}.db")
