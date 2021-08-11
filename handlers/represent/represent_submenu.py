b63d0de52339b4fb4893eef02f95d21945bd5ee5
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-11 20:10:47 +0300

    Updated handler.

--- handlers/represent/represent_submenu.py
+++ handlers/represent/represent_submenu.py
@@ -1,12 +1,13 @@
 from aiogram.dispatcher.filters import Text
 from aiogram.types import Message
 
-from states import Menus
-from markups.text import menu, buttons
 from loader import dp
+from markups.text import menu, buttons
+from states import Menus
 
 
-@dp.message_handler(Text(equals=buttons.represent_btn.text), state=[Menus.main_menu, None])
+@dp.message_handler(Text(equals=[buttons.represent_btn.text, buttons.go_back_btn.text]),
+                    state=[Menus.main_menu, Menus.representation_months, Menus.representation_months, None])
 async def represent_submenu(message: Message):
     await Menus.represent_submenu.set()
     await message.answer(f"В каком виде представить данные?", reply_markup=menu.represent_submenu)
