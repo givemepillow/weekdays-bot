433085f411f3ee41941f5db686adfaed663f9855
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-11 15:38:07 +0300

    Created on_startup init function with db.create.

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 from aiogram import executor
 
 from handlers import dp
-from loader import bot, storage
+from loader import bot, storage, db
 
 
-async def on_shutdown():
+async def on_shutdown(*args):
     await bot.close()
     await storage.close()
 
 
+async def on_startup(*args):
+    await db.create()
+
+
 if __name__ == '__main__':
-    executor.start_polling(dp, on_shutdown=on_shutdown)
+    executor.start_polling(
+        on_shutdown=on_shutdown,
+        on_startup=on_startup,
+        skip_updates=True,
+        dispatcher=dp
+    )
