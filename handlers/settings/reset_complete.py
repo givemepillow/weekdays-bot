1b6093394657743f2c96ab5fd899101bd7f8705b
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-13 20:02:26 +0300

    Function calls are reordered.

--- handlers/settings/reset_complete.py
+++ handlers/settings/reset_complete.py
@@ -8,6 +8,6 @@ from states import Menus
 
 @dp.message_handler(Text(equals=buttons.yes_btn.text), state=Menus.are_u_sure)
 async def reset_complete(message: Message):
+    await message.answer(f"Все выходные сброшены. ☑️", reply_markup=menu.go_back_submenu)
     await Menus.reset_complete.set()
     await db.save_user_holidays(message.from_user.id, [])
-    await message.answer(f"Все выходные сброшены. ☑️", reply_markup=menu.go_back_submenu)
