5cffb411661537c21612f718dc0922595bfd5bc1
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-13 20:20:57 +0300

    New key for calendar id. Also fixed logic.

--- handlers/settings/open_calendar.py
+++ handlers/settings/open_calendar.py
@@ -13,8 +13,9 @@ async def open_calendar(message: Message, state: FSMContext):
     await Menus.calendar.set()
     await message.answer(text="Редактирование выходных.", reply_markup=menu.edit_calendar_submenu)
     user_id = message.from_user.id
+    key = 'calendar_id' + str(user_id)
     async with state.proxy() as data:
         data[user_id] = await db.get_user_holidays(user_id)
         cal_msg = await message.answer(text='Отметьте выходыне:',
                                        reply_markup=await InlineCalendar()(storage=data[user_id]))
-        data['calendar_id'] = cal_msg.message_id
+        data[key] = cal_msg.message_id
--- handlers/settings/settings_submenu.py
+++ handlers/settings/settings_submenu.py
@@ -12,15 +12,17 @@ from states import Menus
                     state=[Menus.calendar, Menus.reset_complete, Menus.are_u_sure])
 @dp.message_handler(Text(equals=buttons.settings_btn.text), state=[Menus.main_menu, None])
 async def settings_submenu(message: Message, state: FSMContext):
+    user_id = message.from_user.id
+    key = 'calendar_id'+str(user_id)
     async with state.proxy() as data:
-        if 'calendar_id' in data and data['calendar_id'] is not None:
-            await bot.delete_message(chat_id=message.chat.id, message_id=data['calendar_id'])
-            data['calendar_id'] = None
-        if message.text == buttons.edit_holidays_save_btn.text:
-            await message.answer(f"Выходные обновлены. 🟢", reply_markup=menu.settings_submenu)
-            await db.save_user_holidays(user_id=message.from_user.id, user_holidays=data[message.from_user.id])
-        elif message.text == buttons.edit_holidays_cancel_btn.text:
-            await message.answer("Последние изменения отменены. ⭕️", reply_markup=menu.settings_submenu)
+        if key in data and data[key] is not None:
+            await bot.delete_message(chat_id=message.chat.id, message_id=data[key])
+            data[key] = None
+            if message.text == buttons.edit_holidays_save_btn.text:
+                await message.answer(f"Выходные обновлены. 🟢", reply_markup=menu.settings_submenu)
+                await db.save_user_holidays(user_id=message.from_user.id, user_holidays=data[message.from_user.id])
+            elif message.text == buttons.edit_holidays_cancel_btn.text:
+                await message.answer("Последние изменения отменены. ⭕️", reply_markup=menu.settings_submenu)
         else:
             await message.answer("Вы перешли в настройки. ⚙️", reply_markup=menu.settings_submenu)
     await Menus.settings_submenu.set()
